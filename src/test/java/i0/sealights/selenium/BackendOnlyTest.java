fa0b8a7ceed492e4359cdb430b55932f2c4009cd
Gregory Futa <gregory.futa@sealights.io>
2023-03-16 09:13:29 +0200

    Selenim working

---
+++ src/test/java/i0/sealights/selenium/BackendOnlyTest.java
@@ -0,0 +1,30 @@
+package i0.sealights.selenium;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+public class BackendOnlyTest {
+
+    final RestTemplate restTemplate = new RestTemplate();
+
+    @Test
+    public void shouldEvaluateTheExpression() {
+        final ResponseEntity<String> actualResponse = restTemplate.getForEntity(
+            "http://localhost:9080/api/evaluate/3+4",
+            String.class);
+
+        assertTrue(actualResponse.getBody().contains("7"));
+    }
+
+    @Test
+    public void shouldSumTheFiniteGeometricSeries() {
+        final ResponseEntity<String> actualResponse = restTemplate.getForEntity(
+            "http://localhost:9080/api/evaluate/3+3",
+            String.class);
+
+        assertTrue(actualResponse.getBody().contains("6"));
+    }
+}
